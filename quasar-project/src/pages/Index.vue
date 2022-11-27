25f97d85c806aacdebdd616274b1370fe0f52084
Roberto S. Santos <robertosilva1991@outlook.com>
2022-11-27 16:52:58 -0300

    Tabela com resultado da api

--- quasar-project/src/pages/Index.vue
+++ quasar-project/src/pages/Index.vue
@@ -1,15 +1,71 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="row">
+      <q-table title="Tabela Dinâmica - Star Wars People" :data="posts" :columns="columns" row-key="name" class="col" separator="cell"/>
+    </div>
+
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      columns: [
+        {
+          name: 'name',
+          label: 'Nome',
+          field: 'name',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'height',
+          label: 'Altura',
+          field: 'height',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'mass',
+          label: 'Peso',
+          field: 'mass',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'birth_year',
+          label: 'Aniversário',
+          field: 'birth_year',
+          align: 'left',
+          sortable: true
+        },
+        {
+          name: 'gender',
+          label: 'Gênero',
+          field: 'gender',
+          align: 'left',
+          sortable: true
+        }
+      ],
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    getPosts () {
+      this.$axios.get('https://swapi.dev/api/people/')
+        .then((res) => {
+          this.posts = res.data.results
+          console.log(res.data.results)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
